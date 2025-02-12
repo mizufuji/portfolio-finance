78d62330725ca4d1e1d739535246b77f0f701caa
mizufuji <future-nature@fuji.waseda.jp>
2025-02-12 11:03:52 +0900

    ヘッダーの編集と会員登録機能を実装

--- financial_asset/accounts/urls.py
+++ financial_asset/accounts/urls.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import path
+from django.views.generic import CreateView
 
 
 urlpatterns = [
+    path(
+        "signup/",
+        CreateView.as_view(
+            template_name="registration/signup.html",
+            form_class=UserCreationForm,
+            success_url="/",
+        ),
+        name="signup",
+    ),
     path(
         "login/",
         LoginView.as_view(template_name="registration/login.html"),
