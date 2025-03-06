d03e7191590ad744a102a8f7f32f9d720d181b35
mizufuji <future-nature@fuji.waseda.jp>
2025-03-06 20:18:35 +0900

    staticfiles_dirsを再度変更

--- financial_asset/financial_asset/settings.py
+++ financial_asset/financial_asset/settings.py
@@ -113,7 +113,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'accounts/static'),
+    os.path.join(BASE_DIR, "accounts/static"),
+    os.path.join(BASE_DIR, "asset/static"), 
 ]
 
 # Default primary key field type
