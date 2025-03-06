d8339686098094f44681cf8ba3843c454accb64e
mizufuji <future-nature@fuji.waseda.jp>
2025-03-06 13:52:16 +0900

    wsgiを変更

--- financial_asset/financial_asset/wsgi.py
+++ financial_asset/financial_asset/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "financial_asset.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "financial_asset.financial_asset.settings"
+)
 
 application = get_wsgi_application()
